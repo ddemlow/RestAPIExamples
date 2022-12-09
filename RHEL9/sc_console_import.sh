920531019b922e6a29198667b11cc03a1c0ca0d1
David Demlow <ddemlow@scalecomputing.com>
2022-12-09 16:46:17 -0500

    Update sc_console_import.sh

--- RHEL9/sc_console_import.sh
+++ RHEL9/sc_console_import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #run script on node console to initiate ubuntu 20_04 import to current cluster
 #could do this using curl, rest api but for now will use sc command
-sc vm import uri "https://github.com/ddemlow/RestAPIExamples/raw/master/rhel9-cloud-init" definitionFile "rhel9-cloud-init.xml" name test wait yes
+sc vm import uri "https://github.com/ddemlow/RestAPIExamples/raw/master/RHEL9" definitionFile "rhel9-cloud-init.xml" name test wait yes
 #note - this vm image needs cloud-init to be useable
